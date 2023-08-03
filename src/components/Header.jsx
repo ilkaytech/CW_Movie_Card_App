70923fb3aad46018275bb844ca76fa95aa57b357
ilkay <06ilkayozturk@gmail.com>
2023-08-03 18:33:06 +0300

    create_headerjsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import Container from "react-bootstrap/Container";
+import { Image } from "react-bootstrap";
+const Header = () => {
+  return (
+    <Container>
+      <Image src="holder.js/171x180" />
+    </Container>
+  );
+};
+
+export default Header;
