d30990630f1120a6ecdb22292092f140b2041e87
ilkay <06ilkayozturk@gmail.com>
2023-08-03 18:49:01 +0300

    create_ContainerCard

---
+++ src/components/ContainerCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Form from "react-bootstrap/Form";
+
+const ContainerCard = () => {
+  return (
+    <>
+      <Form.Control type="text" placeholder="Normal text" />
+    </>
+  );
+};
+
+export default ContainerCard;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import Container from "react-bootstrap/Container";
 import { Image } from "react-bootstrap";
+import logo from "../assets/nba-logo.png";
 const Header = () => {
   return (
     <Container>
-      <Image src="holder.js/171x180" />
+      <Image src={logo} width="200px" />
+      <h1 className="my-2">NBA LEGENDS</h1>
     </Container>
   );
 };
