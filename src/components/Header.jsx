69bdddb91a9428ef5f5cb246995f5bd34bf6de57
ilkay <06ilkayozturk@gmail.com>
2023-08-04 19:01:16 +0300

    edit logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,10 @@
 import Container from "react-bootstrap/Container";
 import { Image } from "react-bootstrap";
-import logo from "../assets/nba-logo.png";
+import logo from "../assets/logo.png";
 const Header = () => {
   return (
     <Container>
-      <Image src={logo} width="200px" />
-      <h1 className="my-2">NBA LEGENDS</h1>
+      <Image src={logo} width="300px" />
     </Container>
   );
 };
